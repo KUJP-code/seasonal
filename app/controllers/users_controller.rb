0b487fc0bed63fa5c6176e9df09e842438beb116
Brett Tanner <bretttanner171@gmail.com>
2022-12-20 14:26:15 +0900

    Create/Style/Translate User#index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
+# Controls flow of info for Users resource
 class UsersController < ApplicationController
+  def index
+    @users = User.all.order(updated_at: :desc).limit(20) if current_user.admin?
+    # Limit AM to Users in their area
+    # @users = current_user.area.users.order(updated_at: :desc).limit(10) if current_user.area_manager?
+    # Limit SM to Users at their school
+    # @users = current_user.school.users.order(updated_at: :desc).limit(10) if current_user.admin?
+  end
 end
