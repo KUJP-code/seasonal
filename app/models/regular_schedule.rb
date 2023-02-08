1ece5c83e44ced85484b137f597584067d5d1174
Brett Tanner <bretttanner171@gmail.com>
2023-02-08 14:18:08 +0900

    Add helper method to actually display days

--- app/models/regular_schedule.rb
+++ app/models/regular_schedule.rb
@@ -2,4 +2,8 @@
 
 class RegularSchedule < ApplicationRecord
   belongs_to :child
+
+  def days
+    "#{'月, ' if monday}#{'火, ' if tuesday}#{'水, ' if wednesday}#{'木, ' if thursday}#{'金' if friday}"
+  end
 end
