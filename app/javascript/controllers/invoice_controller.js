f076be8bcab508a87f7bc289413926e276d3cf7f
Brett Tanner <bretttanner171@gmail.com>
2023-03-02 22:21:58 +0900

    Fix for newly registered arr/dept opts

--- app/javascript/controllers/invoice_controller.js
+++ app/javascript/controllers/invoice_controller.js
@@ -32,8 +32,6 @@ export default class extends Controller {
       }
     }
 
-    console.log('added')
-
     this.dispatch('add')
   }
 
@@ -55,34 +53,34 @@ export default class extends Controller {
   }
 
   radio (child, cost, id, siblings, type) {
+    this.add(child, cost, id, type)
+
     siblings.forEach(sibling => {
       const child = sibling.dataset.registerChildValue
       const id = sibling.dataset.registerIdValue
 
       this.remove(child, id, 'Option')
     });
-
-    this.add(child, cost, id, type)
   }
 
   remove (child, id, type) {
     const wrapper = (type === 'TimeSlot') ? document.getElementById(`slot${id}`.concat(`child${child}`)) : document.getElementById(`opt${id}`.concat(`child${child}`))
 
-    if (wrapper.dataset.newRecord === 'true') {
-      // For newly created registrations
-      wrapper.remove()
-    } else {
-      // For existing registrations
-      const destroy = wrapper.querySelector("input[name*='_destroy']")
-      destroy.value = '1'
-      wrapper.classList.remove(`child${child}`)
-      if (type === 'Option') {
-        const cost = wrapper.querySelector('.opt_cost.hidden')
-        cost.classList.remove('registered')
+    if (wrapper) {
+      if (wrapper.dataset.newRecord === 'true') {
+        // For newly created registrations
+        wrapper.remove()
+      } else {
+        // For existing registrations
+        const destroy = wrapper.querySelector("input[name*='_destroy']")
+        destroy.value = '1'
+        wrapper.classList.remove(`child${child}`)
+        if (type === 'Option') {
+          const cost = wrapper.querySelector('.opt_cost.hidden')
+          cost.classList.remove('registered')
+        }
       }
     }
-
-    console.log('removed')
 
     this.dispatch('remove')
   }
