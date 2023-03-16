688968c89b00ee19a9b25bad5665fa1a8cf8f552
Brett Tanner <bretttanner171@gmail.com>
2023-03-16 17:37:05 +0900

    Fix callback causing issues with invoice.summary

--- app/models/adjustment.rb
+++ app/models/adjustment.rb
@@ -2,16 +2,8 @@
 
 # Represents adjustments to the price of registrations
 class Adjustment < ApplicationRecord
-  after_commit :update_invoice
-
   belongs_to :invoice
 
   # Validations
   validates :reason, :change, presence: true
-
-  private
-
-  def update_invoice
-    invoice.calc_cost
-  end
 end
