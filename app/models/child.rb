f8a6b88fa211ae4d9c891069881e680b132e37f1
Brett Tanner <bretttanner171@gmail.com>
2023-03-16 17:43:20 +0900

    Remove leftover logging statement

--- app/models/child.rb
+++ app/models/child.rb
@@ -91,10 +91,6 @@ class Child < ApplicationRecord
   def full_days(event, invoice_slot_ids)
     full_days = time_slots.where(id: invoice_slot_ids, morning: true, event: event).distinct
 
-    full_days.each do |slot|
-      Rails.logger.debug slot
-    end
-
     full_days.count { |slot| registered?(slot.afternoon_slot) }
   end
 
