98def8cef630a54c64f2e636e3c7906009d33ab9
Brett Tanner <bretttanner171@gmail.com>
2023-03-30 23:03:29 +0900

    Fix typos

--- db/seeds/production/03_customers.rb
+++ db/seeds/production/03_customers.rb
@@ -1,12 +1,12 @@
 School.all.each do |school|
-  20.times.do |i|
+  20.times do |i|
     school.customers.create!(
       first_name: %w[Timmy Sally Billy Sarah Brett ヨシ マリナ カイト].sample,
       family_name: %w[Timmy Sally Billy Sarah Brett ヨシ マリナ カイト].sample,
       kana_first: %w[Timmy Sally Billy Sarah Brett ヨシ マリナ カイト].sample,
       kana_family: %w[Timmy Sally Billy Sarah Brett ヨシ マリナ カイト].sample,
       email: "#{school.name}#{i}@gmail.com",
-      password: 'potato',
+      password: 'mashedpotatoessssss',
       postcode: '216-0011',
       address: 'pizza',
       prefecture: '東京都',
