5ca75c52aed55594bd712cbe385dc56e9ed727c0
Brett Tanner <bretttanner171@gmail.com>
2023-04-06 19:58:36 +0900

    Mandatory 'make x work with turbo' commit

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,12 +4,16 @@
 //= require bootstrap-sprockets
 
 import "@hotwired/turbo-rails";
+import "@popperjs/core";
+import "bootstrap";
 import "controllers";
 
 // Initializers for Bootstrap components that need them
-const popoverTriggerList = document.querySelectorAll(
-  '[data-bs-toggle="popover"]'
-);
-[...popoverTriggerList].map(
-  (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
-);
+document.addEventListener("turbo:load", function () {
+  const popoverTriggerList = document.querySelectorAll(
+    '[data-bs-toggle="popover"]'
+  );
+  [...popoverTriggerList].map(
+    (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+  );
+});
