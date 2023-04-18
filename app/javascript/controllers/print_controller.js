fc56d9a98e3de87bc526658624e1e99aea279d2b
Brett Tanner <bretttanner171@gmail.com>
2023-04-18 19:43:05 +0900

    Print attendance without random space

--- app/javascript/controllers/print_controller.js
+++ app/javascript/controllers/print_controller.js
@@ -1,29 +1,22 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["table", "heading"];
+  static targets = ["print"];
 
   print() {
-    const tables = document.getElementsByTagName("table");
-    const headings = document.getElementsByTagName("h1");
-    // Hide all tables and headings
-    for (let table of tables) {
-      table.classList.add("d-none");
+    const divs = document.querySelectorAll(".slot_attendance");
+    // Hide all other attendance tables
+    for (let div of divs) {
+      div.classList.add("d-none");
     }
-    for (let heading of headings) {
-      heading.classList.add("d-none");
-    }
-    // Reveal target table and heading
-    this.tableTarget.classList.remove("d-none");
-    this.headingTarget.classList.remove("d-none");
-    // Print target table
+
+    // Reveal target table and heading, then print
+    this.printTarget.classList.remove("d-none");
     window.print();
-    // Return all tables and headings to visible
-    for (let table of tables) {
-      table.classList.remove("d-none");
-    }
-    for (let heading of headings) {
-      heading.classList.remove("d-none");
+
+    // Make all attendance tables visible
+    for (let div of divs) {
+      div.classList.remove("d-none");
     }
   }
 }
