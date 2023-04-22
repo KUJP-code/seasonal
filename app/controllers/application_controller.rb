1585d43443e10d8f09d6da8b4bf3531c86d289d0
Brett Tanner <bretttanner171@gmail.com>
2023-04-22 12:05:38 +0900

    Install Pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,26 @@
 
 # Everything here applies to and is accessible from the whole app
 class ApplicationController < ActionController::Base
+  # Enable Pundit on all controllers
+  include Pundit::Authorization
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   # Make sure we're in the right language and know who's making changes
   before_action :switch_locale, :set_paper_trail_whodunnit
 
   private
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 
   def switch_locale
     locale = params[:locale] || I18n.default_locale
     I18n.locale = locale
   end
 
-  def default_url_options
-    { locale: I18n.locale }
+  def user_not_authorized
+    flash[:alert] = t('.not_authorized')
+    redirect_back(fallback_location: root_path)
   end
 end
