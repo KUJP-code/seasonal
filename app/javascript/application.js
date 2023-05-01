866ddf1598563126e1d1b664d826d0ca5034f82d
Brett <bretttanner171@gmail.com>
2023-05-01 15:09:15 +0900

    Add FE login validation

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,6 +8,31 @@ import "controllers";
 
 // Initializers for Bootstrap components that need them
 
+// Form validation
+
+(() => {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
 // Popovers
 
 document.addEventListener("turbo:load", function () {
