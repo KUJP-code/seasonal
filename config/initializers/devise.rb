b0fdcf9122f71ebe0e66a5211111f85f9f111ddf
Brett <bretttanner171@gmail.com>
2023-05-02 14:36:21 +0900

    Configure Devise mailer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -40,13 +40,13 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 
-  %(Kids Up Events <no-reply@eventdbstaging-env.eba-mpbqmmjv.ap-northeast-1.elasticbeanstalk.com>)
+  %(Kids Up Events <bookings@kids-up.app>)
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = 'Devise::Mailer'
+  config.mailer = 'Devise::Mailer'
 
   # Configure the parent class responsible to send e-mails.
-  # config.parent_mailer = 'ActionMailer::Base'
+  config.parent_mailer = 'ActionMailer::Base'
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
