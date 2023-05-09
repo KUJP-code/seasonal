fbf663566d85cfa659039bb5f37cd5d221eb090d
Brett <bretttanner171@gmail.com>
2023-05-09 15:12:26 +0900

    Remove requirement for acct conf b4 access

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -159,8 +159,7 @@ Devise.setup do |config|
   # without confirming their account.
   # Default is 0.days, meaning the user cannot access the website without
   # confirming their account.
-  # Change this when we push to production, maybe 1 day or something?
-  # config.allow_unconfirmed_access_for = 0.days
+  config.allow_unconfirmed_access_for = nil
 
   # A period that the user is allowed to confirm their account before their
   # token becomes invalid. For example, if set to 3.days, the user can confirm
@@ -168,7 +167,7 @@ Devise.setup do |config|
   # their account can't be confirmed with the token any more.
   # Default is nil, meaning there is no restriction on how long a user can take
   # before confirming their account.
-  # config.confirm_within = 7.days
+  config.confirm_within = nil
 
   # If true, requires any email changes to be confirmed (exactly the same way as
   # initial account confirmation) to be applied. Requires additional unconfirmed_email
