d0b028420a1b3244378b2b5a7a9e99090247b920
Brett <bretttanner171@gmail.com>
2023-05-15 14:03:07 +0900

    Add character counters

---
+++ app/javascript/controllers/count_controller.js
@@ -0,0 +1,47 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["input", "counter"];
+  static values = { countdown: Boolean };
+
+  initialize() {
+    this.update = this.update.bind(this);
+  }
+
+  connect() {
+    this.update();
+    this.inputTarget.addEventListener("input", this.update);
+  }
+
+  disconnect() {
+    this.inputTarget.removeEventListener("input", this.update);
+  }
+
+  update() {
+    if (this.countdownValue) {
+      this.counterTarget.innerHTML = `${this.count.toString()} more required`;
+    } else {
+      this.counterTarget.innerHTML = `Current length: ${this.count.toString()}`;
+    }
+  }
+
+  get count() {
+    let value = this.inputTarget.value.length;
+
+    if (this.hasCountdownValue) {
+      if (this.maxLength < 0) {
+        console.error(
+          `[stimulus-character-counter] You need to add a maxlength attribute on the input to use countdown mode. The current value is: ${this.maxLength}.`
+        );
+      }
+
+      value = Math.max(this.maxLength - value, 0);
+    }
+
+    return value;
+  }
+
+  get maxLength() {
+    return this.inputTarget.maxLength;
+  }
+}
