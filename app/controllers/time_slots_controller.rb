032658a797b70916b2288ac92b14519b0f9eaddc
Brett <bretttanner171@gmail.com>
2023-05-17 11:49:05 +0900

    Paginate time slots

--- app/controllers/time_slots_controller.rb
+++ app/controllers/time_slots_controller.rb
@@ -4,7 +4,7 @@
 class TimeSlotsController < ApplicationController
   def index
     authorize(TimeSlot)
-    @events = policy_scope(TimeSlot).order(:start_date)
+    @events = policy_scope(TimeSlot).page(params[:page]).per(1)
   end
 
   def show
