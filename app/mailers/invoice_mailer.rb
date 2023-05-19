71f86920f8166d5123728451b9c77243262ff27c
Brett <bretttanner171@gmail.com>
2023-05-19 16:30:19 +0900

    Add conditional for updates by SM

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -10,14 +10,16 @@ class InvoiceMailer < ApplicationMailer
 
   def updated_notif
     @invoice = params[:invoice]
+    @updater = User.find(@invoice.versions.last.whodunnit)
     @parent = @invoice.child.parent
     mail(to: @parent.email, subject: t('.invoice_updated'))
   end
 
   def sm_updated_notif
     @invoice = params[:invoice]
+    @updater = User.find(@invoice.versions.last.whodunnit)
     @parent = @invoice.child.parent
-    @sm = @invoice.school.managers.first || User.new(name: 'Emperor Leroy', email: 'h-leroy@kids-up.jp')
+    @sm = @invoice.school.managers.first || User.new(name: 'Leroy', email: 'h-leroy@kids-up.jp')
     mail(to: @sm.email, subject: t('.invoice_updated'))
   end
 end
--- spec/mailers/previews/invoice_preview.rb
+++ spec/mailers/previews/invoice_preview.rb
@@ -3,14 +3,14 @@
 # Preview all emails at http://localhost:3000/rails/mailers/invoice
 class InvoicePreview < ActionMailer::Preview
   def confirmation_notif
-    InvoiceMailer.with(invoice: Invoice.all.sample(1).first, user: User.first).confirmation_notif
+    InvoiceMailer.with(invoice: Invoice.all.order(:updated_at).last, user: User.first).confirmation_notif
   end
 
   def updated_notif
-    InvoiceMailer.with(invoice: Invoice.all.sample(1).first, user: User.first).updated_notif
+    InvoiceMailer.with(invoice: Invoice.all.order(:updated_at).last, user: User.first).updated_notif
   end
 
   def sm_updated_notif
-    InvoiceMailer.with(invoice: Invoice.all.sample(1).first, user: User.first).sm_updated_notif
+    InvoiceMailer.with(invoice: Invoice.all.order(:updated_at).last, user: User.first).sm_updated_notif
   end
 end
