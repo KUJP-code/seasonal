dc8fbae1a157a71ba4e5fc8438d7ef8f8622664f
Brett Tanner <bretttanner171@gmail.com>
2023-05-19 23:03:58 +0900

    Stop trying to load an image from my PC -__-

--- spec/mailers/previews/auth_mailer_preview.rb
+++ spec/mailers/previews/auth_mailer_preview.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Preview all emails at http://localhost:3000/rails/mailers/auth_mailer
 class AuthMailerPreview < ActionMailer::Preview
   def confirmation_instructions
     AuthMailer.confirmation_instructions(User.first, "faketoken")
