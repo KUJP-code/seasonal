57e9e95d369beab320198c5683e4c198699cd42f
Brett Tanner <bretttanner171@gmail.com>
2023-05-21 22:19:32 +0900

    Add copy button to email template

---
+++ app/javascript/controllers/clipboard_controller.js
@@ -0,0 +1,42 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["button", "source"];
+  static values = {
+    successDuration: {
+      type: Number,
+      default: 2000,
+    },
+  };
+
+  connect() {
+    if (!this.hasButtonTarget) return;
+
+    this.originalContent = this.buttonTarget.innerHTML;
+  }
+
+  copy(event) {
+    event.preventDefault();
+
+    const text = this.sourceTarget.innerHTML || this.sourceTarget.value;
+    const strippedText = text
+      .replace(/(<([^>]+)>)/gi, "")
+      .replace("コピー", "");
+
+    navigator.clipboard.writeText(strippedText).then(() => this.copied());
+  }
+
+  copied() {
+    if (!this.hasButtonTarget) return;
+
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+    }
+
+    this.buttonTarget.innerHTML = this.data.get("successContent");
+
+    this.timeout = setTimeout(() => {
+      this.buttonTarget.innerHTML = this.originalContent;
+    }, this.successDurationValue);
+  }
+}
