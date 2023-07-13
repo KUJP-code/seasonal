c27ea11efb92dbe8564a608c3bf79f4f1dd3da2b
Brett Tanner <bretttanner171@gmail.com>
2023-07-14 01:25:48 +0900

    Use total cost rather than associations

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -4,9 +4,10 @@
 class ChartsController < ApplicationController
   def index
     authorize(:chart)
-    @invoices = Invoice.real
+    @invoices = Invoice.where('total_cost > 3000')
     @children = Child.joins(:real_invoices).distinct
     @slot_registrations = Registration.all.where(registerable_type: 'TimeSlot')
+    @school_hash = School.all.to_h { |school| [school.id, school.name] }
   end
 
   def show; end
