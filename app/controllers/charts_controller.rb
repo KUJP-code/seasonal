f41b7591ea6c9b6188431ef577537f4d2feb93b1
Brett Tanner <bretttanner171@gmail.com>
2023-07-14 13:31:06 +0900

    Add colors & activity popularity chart

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -8,6 +8,7 @@ class ChartsController < ApplicationController
     @children = Child.where.not(school_id: TEST_SCHOOLS).joins(:real_invoices).distinct
     @slot_registrations = Registration.all.where(registerable_type: 'TimeSlot', child_id: @children.ids)
     @school_hash = School.where.not(id: TEST_SCHOOLS).to_h { |school| [school.id, school.name] }
+    @time_slots = TimeSlot.where(morning: true, category: %i[seasonal outdoor]).or(TimeSlot.where(category: :special))
   end
 
   def show; end
