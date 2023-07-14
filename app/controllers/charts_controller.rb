9247e0918ae39aaa2d2caae8c46707a8353a7368
Brett Tanner <bretttanner171@gmail.com>
2023-07-14 12:59:09 +0900

    Remove test schools from stats

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -4,11 +4,13 @@
 class ChartsController < ApplicationController
   def index
     authorize(:chart)
-    @invoices = Invoice.where('total_cost > 3000')
-    @children = Child.joins(:real_invoices).distinct
-    @slot_registrations = Registration.all.where(registerable_type: 'TimeSlot')
-    @school_hash = School.all.to_h { |school| [school.id, school.name] }
+    @invoices = Invoice.where('total_cost > 3000').where.not(event_id: TEST_SCHOOLS)
+    @children = Child.where.not(school_id: TEST_SCHOOLS).joins(:real_invoices).distinct
+    @slot_registrations = Registration.all.where(registerable_type: 'TimeSlot', child_id: @children.ids)
+    @school_hash = School.where.not(id: TEST_SCHOOLS).to_h { |school| [school.id, school.name] }
   end
 
   def show; end
+
+  TEST_SCHOOLS = [1, 2].freeze
 end
