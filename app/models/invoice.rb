943fa3e3df26b5ef3f6ffb402f4b1226b58c66c2
Brett <bretttanner171@gmail.com>
2023-08-09 12:42:03 +0900

    Exclude diff matsuri from special cost

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -132,7 +132,7 @@ class Invoice < ApplicationRecord
     # Add cost due to special day registrations
     # Now also has to handle Minami Machida's dumb different special day
     # Morning has no extra cost, but afternoon is 1100 for some reason
-    special_count = slot_regs.count { |reg| !reg._destroy && reg.registerable.special? && %w[バンダナの絞り染め 夏祭り].exclude?(reg.registerable.name) }
+    special_count = slot_regs.count { |reg| !reg._destroy && reg.registerable.special? && %w[バンダナの絞り染め 夏祭り@南町田グランベリーパーク 夏祭り@二俣川].exclude?(reg.registerable.name) }
     # Add the 1100 if MM's or Futatamagawa's afternoon special day registered
     summer_names = %w[夏祭り@南町田グランベリーパーク 夏祭り@二俣川]
     summer_festival = [18, 21].include?(event_id) && slot_regs.any? { |reg| summer_names.include?(reg.registerable.name) }
