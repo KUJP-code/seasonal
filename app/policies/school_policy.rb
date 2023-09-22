a5aaa48139daade0e2b314dec4de22ec5474277e
Brett Tanner <bretttanner171@gmail.com>
2023-09-22 12:59:32 +0900

    Scope schools users can see stats for

--- app/policies/school_policy.rb
+++ app/policies/school_policy.rb
@@ -4,14 +4,19 @@
 class SchoolPolicy < ApplicationPolicy
   def show?
     user.admin? ||
-    user.area_manager? ||
-    (user.school_manager? && user.managed_schools.ids.include?(record.id))
+      user.area_manager? ||
+      (user.school_manager? && user.managed_schools.ids.include?(record.id))
   end
 
+  # Decides which schools each role can see stats for
   class Scope < Scope
-    # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      case user.role
+      when 'admin'
+        School.real.order(:id)
+      when 'area_manager'
+        School.real.where(id: user.area_schools.ids).order(:id)
+      end
+    end
   end
 end
