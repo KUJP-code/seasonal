38c197eb724cc47db4915890d03dd3909e98cfa7
Brett Tanner <bretttanner171@gmail.com>
2023-09-22 15:38:51 +0900

    Create show_activities view

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -3,11 +3,11 @@
 # Provides data for the charts pages
 class ChartsController < ApplicationController
   TEST_SCHOOLS = [1, 2].freeze
-
-# TODO: decide schools based on whether it's an area manager or admin. Probably don't set it at all if an SM
+  CATEGORIES = %w[activities bookings children coupons edits options].freeze
 
   def index
     authorize(:chart)
+    @nav = nav_data('index')
     @invoices = Invoice.where('total_cost > 3000').where.not(event_id: TEST_SCHOOLS).includes(:child)
     @coupons = Coupon.where(couponable_id: @invoices.ids)
     @children = Child.where.not(school_id: TEST_SCHOOLS).joins(:real_invoices).distinct
@@ -18,33 +18,81 @@ class ChartsController < ApplicationController
   end
 
   def show
-    @schools = School.real
-    @school = School.find(params[:id])
+    @nav = nav_data('show')
+    @data = send("#{@nav[:category]}_data")
+    render "show_#{@nav[:category]}"
   end
 
   private
 
-  def activity_data
-    
+  def activities_data
+    school = @nav[:school]
+
+    if school.id.zero?
+      activities_all_data
+    else
+      activities_school_data(school)
+    end
   end
 
-  def booking_data
-    
+  def activties_all_data
+    event_ids = Event.upcoming.ids
+    school_ids = School.real.ids
+    slots = TimeSlot.where(id: school_ids, event_id: event_ids)
+
+    {
+      activities: slots.morning.or(slots.special),
+      afternoons: slots.afternoon.where.not(category: :special),
+      slots: slots
+    }
   end
 
-  def child_data
-    
+  def activities_school_data(school)
+    event = school.events.find_by(name: @nav[:event])
+
+    {
+      activities: event.time_slots.morning.or(event.time_slots.special),
+      afternoons: event.time_slots.afternoon.where.not(category: :special),
+      slots: event.time_slots
+    }
   end
 
-  def coupon_data
-    
+  def bookings_data
+    "worked"
   end
 
-  def edit_data
-    
+  def children_data
+    "worked"
   end
 
-  def option_data
-    
+  def coupons_data
+    "worked"
+  end
+
+  def edits_data
+    "worked"
+  end
+
+  def nav_data(action)
+    {
+      category: nav_category,
+      categories: CATEGORIES,
+      event: params[:event] || Event.last.name,
+      events: Event.all.pluck(:name).uniq,
+      schools: policy_scope(School),
+      school: nav_school(action)
+    }
+  end
+
+  def nav_category
+    CATEGORIES.find { |c| c == params[:category] } || CATEGORIES[0]
+  end
+
+  def nav_school(action)
+    action == 'show' ? authorize(School.find(params[:id])) : School.new(id: 0)
+  end
+
+  def options_data
+    "worked"
   end
 end
