f7ef6956f59827d65e5105af927423a29600f6b7
Brett Tanner <bretttanner171@gmail.com>
2023-09-25 19:43:27 +0900

    Refactor categories to partials

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -7,20 +7,20 @@ class ChartsController < ApplicationController
 
   def index
     authorize(:chart)
+    # @nav = nav_data('index')
+    # @invoices = Invoice.where('total_cost > 3000').where.not(event_id: TEST_SCHOOLS).includes(:child)
+    # @coupons = Coupon.where(couponable_id: @invoices.ids)
+    # @children = Child.where.not(school_id: TEST_SCHOOLS).joins(:real_invoices).distinct
+    # @slot_registrations = Registration.all.where(registerable_type: 'TimeSlot', child_id: @children.ids)
+    # @school_hash = School.real.to_h { |school| [school.id, school.name] }
+    # @time_slots = TimeSlot.where(morning: true, category: %i[seasonal outdoor]).or(TimeSlot.where(category: :special))
+    # @versions = PaperTrail::Version.where(item_type: 'Invoice', event: 'update')
     @nav = nav_data('index')
-    @invoices = Invoice.where('total_cost > 3000').where.not(event_id: TEST_SCHOOLS).includes(:child)
-    @coupons = Coupon.where(couponable_id: @invoices.ids)
-    @children = Child.where.not(school_id: TEST_SCHOOLS).joins(:real_invoices).distinct
-    @slot_registrations = Registration.all.where(registerable_type: 'TimeSlot', child_id: @children.ids)
-    @school_hash = School.real.to_h { |school| [school.id, school.name] }
-    @time_slots = TimeSlot.where(morning: true, category: %i[seasonal outdoor]).or(TimeSlot.where(category: :special))
-    @versions = PaperTrail::Version.where(item_type: 'Invoice', event: 'update')
   end
 
   def show
     @nav = nav_data('show')
     @data = send("#{@nav[:category]}_data")
-    render "show_#{@nav[:category]}"
   end
 
   private
