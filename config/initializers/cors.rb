e796c54e9578ad30270903ee583b2e93b14b835e
Brett <bretttanner171@gmail.com>
2023-11-01 15:50:07 +0900

    Allow inquiries to be created with category

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,5 +5,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '/gas_inquiries', headers: :any, methods: %i[get post]
     resource '/gas_update', headers: :any, methods: %i[post]
     resource '/schools', headers: :any, methods: %i[get post]
+    resource '/create_inquiry', headers: :any, methods: %i[post]
   end
 end
