a7bf803ffd7fb474e0c8de17e51d3384f8bd31df
Brett <bretttanner171@gmail.com>
2023-11-15 21:40:53 +0900

    Remove custom error pages

--- config/application.rb
+++ config/application.rb
@@ -26,10 +26,6 @@ module DbPrototypeV2
     config.load_defaults 7.0
     config.active_storage.variant_processor = :mini_magick
 
-    config.exceptions_app = ->(env) {
-      ErrorsController.action(:show).call(env)
-    }
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
