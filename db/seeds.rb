2934151e3ded11d3f7fac5737f866109783f0aa0
Brett <bretttanner171@gmail.com>
2024-01-26 18:50:14 +0900

    Remove useless seeds

--- db/seeds.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-Dir[Rails.root.join('db', 'seeds', '*.rb')].sort.each do |seed|
-  load seed
-end
-
-Dir[Rails.root.join('db', 'seeds', Rails.env, '*.rb')].sort.each do |seed|
-  load seed
-end
