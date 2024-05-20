43bbbbba797048c95ca6e191c77ae6038e2cd92b
Brett Tanner <bretttanner171@gmail.com>
2024-05-20 13:27:25 +0900

    Refactor User form

--- app/javascript/controllers/conditional_controller.js
+++ app/javascript/controllers/conditional_controller.js
@@ -1,61 +1,59 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["target", "condition"];
-  static values = {
-    pin: String,
-  };
+	static targets = ["target", "condition"];
+	static values = {
+		pin: String,
+	};
 
-  connect() {
-    this.toggle;
-  }
+	connect() {
+		this.toggle;
+	}
 
-  allergy() {
-    const allergy = this.conditionTarget.value;
-    const target = this.targetTarget;
+	allergy() {
+		const allergy = this.conditionTarget.value;
+		const target = this.targetTarget;
 
-    switch (allergy) {
-      case "なし":
-        target.value = "なし";
-        target.readOnly = true;
-        break;
-      case "有":
-        target.removeAttribute("readOnly");
-        target.value = "";
-        break;
-      default:
-        target.value = null;
-        target.readOnly = true;
-        break;
-    }
-  }
+		switch (allergy) {
+			case "なし":
+				target.value = "なし";
+				target.readOnly = true;
+				break;
+			case "有":
+				target.removeAttribute("readOnly");
+				target.value = "";
+				break;
+			default:
+				target.value = null;
+				target.readOnly = true;
+				break;
+		}
+	}
 
-  allowSubmit() {
-    const condition = this.conditionTarget.checked;
-    const target = this.targetTarget;
+	allowSubmit() {
+		const condition = this.conditionTarget.checked;
+		const target = this.targetTarget;
 
-    if (condition) {
-      target.removeAttribute("disabled");
-    } else {
-      target.setAttribute("disabled", true);
-    }
-  }
+		if (condition) {
+			target.removeAttribute("disabled");
+		} else {
+			target.setAttribute("disabled", true);
+		}
+	}
 
-  pin() {
-    const pin = this.conditionTarget.value;
-    if (pin !== this.pinValue) {
-      return;
-    }
+	pin() {
+		const pin = this.conditionTarget.value;
+		if (pin !== this.pinValue) {
+			return;
+		}
 
-    this.targetTargets.forEach((target) => {
-      target.classList.toggle("d-none");
-    });
+		for (const target of this.targetTargets) {
+			target.classList.toggle("d-none");
 
-    // Auto-hide again after 30min
-    setTimeout(() => {
-      this.targetTargets.forEach((target) => {
-        target.classList.toggle("d-none");
-      });
-    }, 1800000);
-  }
+			// Auto-hide after 10min
+			setTimeout(() => {
+				target.classList.toggle("d-none");
+			}, 600000);
+		}
+	}
 }
