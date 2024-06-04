b71075c64c726ba90b441624db4de91c54c27081
Brett <bretttanner171@gmail.com>
2024-06-04 13:05:21 +0900

    Remove Clarity & validate on turbo:load

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -12,61 +12,41 @@ import "Chart.bundle";
 
 // Form validation
 
-(() => {
-  "use strict";
+document.addEventListener("turbo:load", () => {
+	// Fetch all the forms we want to apply custom Bootstrap validation styles to
+	const forms = document.querySelectorAll(".needs-validation");
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll(".needs-validation");
+	// Loop over them and prevent submission
+	for (const form of forms) {
+		form.addEventListener(
+			"submit",
+			(event) => {
+				if (!form.checkValidity()) {
+					event.preventDefault();
+					event.stopPropagation();
+				}
 
-  // Loop over them and prevent submission
-  Array.from(forms).forEach((form) => {
-    form.addEventListener(
-      "submit",
-      (event) => {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-
-        form.classList.add("was-validated");
-      },
-      false
-    );
-  });
-})();
+				form.classList.add("was-validated");
+			},
+			false,
+		);
+	}
+});
 
 // Popovers
 
-document.addEventListener("turbo:load", function () {
-  const popoverTriggerList = document.querySelectorAll(
-    '[data-bs-toggle="popover"]'
-  );
-  [...popoverTriggerList].map(
-    (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
-  );
+document.addEventListener("turbo:load", () => {
+	const popoverTriggerList = document.querySelectorAll(
+		'[data-bs-toggle="popover"]',
+	);
+	[...popoverTriggerList].map(
+		(popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl),
+	);
 });
 
 // Toasts
 
-document.addEventListener("turbo:load", function () {
-  const toastElList = document.querySelectorAll(".toast");
-  [...toastElList].map((toastEl) => new bootstrap.Toast(toastEl).show());
+document.addEventListener("turbo:load", () => {
+	const toastElList = document.querySelectorAll(".toast");
+	[...toastElList].map((toastEl) => new bootstrap.Toast(toastEl).show());
 });
-
-// Tracking from Microsoft Clarity
-
-document.addEventListener(
-  "turbo:load",
-  (function (c, l, a, r, i, t, y) {
-    c[a] =
-      c[a] ||
-      function () {
-        (c[a].q = c[a].q || []).push(arguments);
-      };
-    t = l.createElement(r);
-    t.async = 1;
-    t.src = "https://www.clarity.ms/tag/" + i;
-    y = l.getElementsByTagName(r)[0];
-    y.parentNode.insertBefore(t, y);
-  })(window, document, "clarity", "script", "hghp07f095")
-);
