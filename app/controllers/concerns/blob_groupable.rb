f9dde9f57f32caec5b127cb6c2fc0cf64d18d052
Brett <bretttanner171@gmail.com>
2024-09-02 14:06:17 +0900

    Hide old images from & refactor BlobGroupable

--- app/controllers/concerns/blob_groupable.rb
+++ app/controllers/concerns/blob_groupable.rb
@@ -6,23 +6,28 @@ module BlobGroupable
   included do
     # Returns a hash of 'parent_folder/subfolder' => [[filename, id], [filename, id]]
     # Like { time_slots/summer_2023 => [[fruit_smoothie.avif, 1]] }
+    # So they can be grouped in a select input
     def blobs_by_folder(parent_folder)
       blobs = ActiveStorage::Blob.where('key LIKE ?', "%#{parent_folder}%")
-                                 .map { |blob| [blob.key, blob.id] }
-      paths = blobs.to_h { |b| [subfolder(b.first), []] }
-
-      blobs.each do |b|
-        paths[subfolder(b.first)]
-          .push([filename(b.first), b.last])
-      end
-
-      paths
+                                 .where(created_at: 2.months.ago..Time.zone.now)
+                                 .order(created_at: :desc).pluck(:key, :id)
+      group_by_path(blobs)
     end
   end
 
   private
 
-  def subfolder(key)
+  def group_by_path(blobs)
+    paths = blobs.to_h { |key, _id| [path(key), []] }
+
+    blobs.each do |key, id|
+      paths[path(key)].push([filename(key), id])
+    end
+
+    paths
+  end
+
+  def path(key)
     key.split('/')[0..-2].join('/')
   end
 
--- spec/concerns/blob_groupable_spec.rb
+++ spec/concerns/blob_groupable_spec.rb
@@ -5,16 +5,31 @@ require 'rails_helper'
 RSpec.describe BlobGroupable do
   include ActiveSupport::Testing::TimeHelpers
 
-  it 'returns image filenames in passed folder, grouped by subfolder' do
-    ActiveStorage::Blob.create_and_upload!(
-      io: Rails.root.join('app/assets/images/fruit_smoothie.avif').open,
-      filename: 'fruit_smoothie.avif',
-      key: 'time_slots/summer_2023/fruit_smoothie.avif'
-    )
-    images = TimeSlotsController.new.blobs_by_folder('time_slots')
-    expect(images).to eq({ 'time_slots/summer_2023' => [['fruit_smoothie.avif', 1]] })
-  end
+  [EventsController, TimeSlotsController].each do |controller|
+    context "when used in #{controller}" do
+      it 'returns image filenames in passed folder, grouped by subfolder' do
+        parent_folder = controller.class.name.underscore.tr('controller', '')
+        blob = ActiveStorage::Blob.create_and_upload!(
+          io: Rails.root.join('app/assets/images/fruit_smoothie.avif').open,
+          filename: 'fruit_smoothie.avif',
+          key: "#{parent_folder}/summer_2023/fruit_smoothie.avif"
+        )
+        images = controller.new.blobs_by_folder(parent_folder)
+        expect(images).to eq({ "#{parent_folder}/summer_2023" =>
+                               [['fruit_smoothie.avif', blob.id]] })
+      end
 
-  it 'does not return images uploaded > 3 months ago' do
+      it 'does not return images uploaded > 2 months ago' do
+        parent_folder = controller.class.name.underscore.tr('controller', '')
+        ActiveStorage::Blob.create_and_upload!(
+          io: Rails.root.join('app/assets/images/fruit_smoothie.avif').open,
+          filename: 'fruit_smoothie.avif',
+          key: "#{parent_folder}/summer_2023/fruit_smoothie.avif"
+        )
+        travel_to(2.months.from_now + 1.day)
+        images = controller.new.blobs_by_folder(parent_folder)
+        expect(images).to be_blank
+      end
+    end
   end
 end
