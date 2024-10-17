1092edf5320ef5f909708ef8a9a68fe492393d63
jaysonpye <jaysonpye@hotmail.com>
2024-10-17 19:46:02 +0900

    specify kidsup in coors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,8 +4,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '/gas_schools', headers: :any, methods: %i[get]
     resource '/gas_inquiries', headers: :any, methods: %i[get post]
     resource '/gas_update', headers: :any, methods: %i[post]
-    resource '/schools', headers: :any, methods: %i[post]
     resource '/setsu_schools.json', headers: :any, methods: %i[get]
     resource '/create_inquiry.json', headers: :any, methods: %i[post]
+  end
+  allow do
+    origins 'https://kids-up.jp'
+    resource '/schools', headers: :any, methods: %i[post]
   end
 end
