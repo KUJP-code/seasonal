43c458b3026aae6a8417c4e15de5ff2249c66fc8
jaysonpye <jaysonpye@hotmail.com>
2025-10-08 10:35:37 +0900

    add helper to provide proper closed messaging

--- app/helpers/time_slots_helper.rb
+++ app/helpers/time_slots_helper.rb
@@ -11,4 +11,11 @@ module TimeSlotsHelper
   def times(slot)
     "#{f_time(slot.start_time)} - #{f_time(slot.end_time)}"
   end
+
+  def slot_closed_key(slot)
+    return 'am_closed' if slot.respond_to?(:morning?) && slot.morning?
+    return 'pm_closed' if slot.respond_to?(:afternoon?) && slot.afternoon?
+
+    'closed'
+  end
 end
