e7e777b7085f378f4428216d6d71b06c3f979ded
jaysonpye <jaysonpye@hotmail.com>
2026-03-27 15:12:29 +0900

    add a mega games card

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,70 +1,71 @@
 # frozen_string_literal: true
 
 module EventsHelper
-  # Schools that use the 21st and special image
-  EXTERNAL_EVENT_SPECIAL_SCHOOL_IDS = [7, 9, 10, 3].freeze
+  MEGA_GAME_MAY_23_SCHOOL_IDS = [
+    4, 5, 6, 8, 11, 13, 14, 15, 17, 18, 20, 22, 23, 25, 27, 28, 32, 33, 34,
+    36, 39, 41
+  ].freeze
+  MEGA_GAME_MAY_24_SCHOOL_IDS = [
+    3, 7, 9, 10, 12, 16, 19, 21, 24, 26, 29, 30, 31, 35, 40
+  ].freeze
 
-  def show_christmas_event?(child = external_event_child)
-    return false unless child
-
-    Time.zone.today <= external_event_date(child)
-  end
-
-  def external_event_child
+  def event_card_child
     current_user&.children&.first
   end
 
-  def external_event_school(child = external_event_child)
+  def event_card_school(child = event_card_child)
     child&.school
   end
 
-  def external_event_school_id(child = external_event_child)
-    child&.school_id
+  def show_mega_game?(child = event_card_child)
+    return false unless child
+
+    hide_on = mega_game_hide_on(child)
+    hide_on.present? && Time.zone.today < hide_on
   end
 
-  def external_event_is_special?(child = external_event_child)
-    EXTERNAL_EVENT_SPECIAL_SCHOOL_IDS.include?(external_event_school_id(child))
-  end
-
-  # Dec 21 for special IDs; Dec 20 otherwise. No date if no child.
-  def external_event_date(child = external_event_child)
+  def mega_game_date(child = event_card_child)
     return nil unless child
 
-    year = 2025
-    day  = external_event_is_special?(child) ? 21 : 20
-    Date.new(year, 12, day)
+    case child.school_id
+    when *MEGA_GAME_MAY_23_SCHOOL_IDS
+      Date.new(2026, 5, 23)
+    when *MEGA_GAME_MAY_24_SCHOOL_IDS
+      Date.new(2026, 5, 24)
+    end
   end
 
-  # Title: always "クリスマス2025", optionally append school in parentheses.
-  def external_event_title(child = external_event_child)
-    base = 'クリスマス2025'
-    school = external_event_school(child)
+  def mega_game_hide_on(child = event_card_child)
+    event_date = mega_game_date(child)
+    event_date ? event_date + 1.day : nil
+  end
+
+  def mega_game_title(child = event_card_child)
+    base = 'Mega Game 2026'
+    school = event_card_school(child)
     school ? "#{base} (#{I18n.t("schools.#{school.name}")})" : base
   end
 
-  # URL
-  def external_event_url
-    'https://kids-up.jp/events/christmas-2025/form/'
+  def mega_game_url
+    'https://my.ptsc.jp/kids-up/entry?i=Mega2026'
   end
 
-  def external_event_image_path(child = external_event_child)
-    if external_event_is_special?(child)
-      asset_path('christmas_special.png')
+  def mega_game_image_path(child = event_card_child)
+    case mega_game_date(child)
+    when Date.new(2026, 5, 23)
+      asset_path('mega_game_23.webp')
+    when Date.new(2026, 5, 24)
+      asset_path('mega_game_24.webp')
     else
-      asset_path('christmas_default.png')
+      ''
     end
   rescue StandardError
     ''
   end
 
-  def external_event_avif_path(child = external_event_child)
-    if external_event_is_special?(child)
-      asset_path('christmas_special.avif')
-    else
-      asset_path('christmas_default.avif')
-    end
+  def mega_game_avif_path(_child = event_card_child)
+    ''
   rescue StandardError
     ''
   end
-
 end
